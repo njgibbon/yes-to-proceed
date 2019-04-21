2f682343be7946b34bf6bf6de78e86df0b687566
njgibbon <njgibbon@outlook.com>
2019-04-21 20:39:49 +0100

    readme and bash implementation of the little tool

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # yes-to-proceed
 confirmation prompt for cli tools
+
+Simple re-usable implementations of a confirmation or exit prompt.
+Common feature of a CLI tool especially when the script causes changes or deletions. 
+
+## bash
+```
+#if implementation
+bash yes_to_proceed_0.sh
+#case implementation
+bash yes_to_proceed_1.sh
+```
+
+## python
+```
+python3 yes_to_proceed.py
+```
+
+## blog
+
+
+## resources
+https://stackoverflow.com/questions/226703/how-do-i-prompt-for-yes-no-cancel-input-in-a-linux-shell-script
+
+https://stackoverflow.com/questions/1885525/how-do-i-prompt-a-user-for-confirmation-in-bash-script
---
+++ bash/yes_to_proceed_0.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function main
+{
+    echo ""
+    checks
+    yes_to_proceed
+    primary_logic
+    echo ""
+}
+
+
+function checks
+{
+    echo ""
+    echo "In checks()"
+    echo "Perform checks on input and other preparation steps."
+    echo ""
+}
+
+function yes_to_proceed
+{
+    echo ""
+    echo "In yes_to_proceed()"
+    echo ""
+    read -p "Are you sure you want to continue? Enter \"yes\":  " answer
+    echo ""
+    if [ "$answer" == yes ] ;then
+        echo "\"yes\" input."
+    else
+        echo "Other than \"yes\" input."
+        echo "Exiting."
+        exit 1
+    fi
+    echo ""
+} 
+
+function primary_logic
+{
+    echo ""
+    echo "In primary_logic()"
+    echo "Execute the primary logic of the script."
+    echo "This function is only entered if \"yes\" was chosen."
+    echo ""
+}
+
+main
